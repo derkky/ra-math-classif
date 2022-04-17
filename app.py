6799d2e536bad21984c85e3c4c8663c859fa6176
derkky <don_foh@hotmail.com>
2022-04-17 12:01:58 +0800

    developed client

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, send_from_directory
 from api.algo import predict_label
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="client", static_url_path='')
 
 @app.route("/")
 def render_client():
-    return send_from_directory("client", "index.html")
+    return send_from_directory(app.static_folder, "index.html")
 
 @app.route("/api/getlabel", methods=["POST"])
 def get_label():
